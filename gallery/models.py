96a777f84eae6b58a3d57b33f07bd8dcfa5bfa5e
Nolfurh <yurii.hliuza@gmail.com>
2023-05-23 09:56:07 +0300

    Add models

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField()
+    categories = models.ManyToManyField(Category)
+    created_date = models.DateField()
+    age_limit = models.PositiveIntegerField()
